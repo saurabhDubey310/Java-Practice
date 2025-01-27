d4976d0cccc9f13f579815d43e47f581580a4d31
saurabhdubey310 <dubeysaurabh310@gmail.com>
2025-01-27 13:46:09 +0530

    added few more files

---
+++ float.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String args[]){
+        float num1 = 3.14F;
+        float num2 = 2.55F;
+        float sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
---
+++ string_concat.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String args[]){
+        String name = "saurabh";
+        String surname = "dubey";
+        String fullname = name + " " + surname;
+        System.out.println("hello Iam" + " " + fullname);
+    }
+}
