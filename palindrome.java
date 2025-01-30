99e6effeb3eab59e477dda3110878578298bc007
saurabhdubey310 <dubeysaurabh310@gmail.com>
2025-01-30 16:30:39 +0530

    few files added

---
+++ palindrome.java
@@ -0,0 +1,17 @@
+public class palindrome {
+
+    public static void main(String[] args) {
+
+        int a=122,b,i;
+        int j = String.valueOf(a).length();
+        int c=0;
+        for(i=1;i<=j;i++){
+            b=a%10;
+            a=a/10;
+
+            c= (10*c) + b;
+        }
+        System.out.println(c);
+
+    }
+}
