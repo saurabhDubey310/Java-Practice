ffe628eac1ed69a22b0ff28a86bb1b968a21e35c
saurabhdubey310 <dubeysaurabh310@gmail.com>
2025-01-30 16:42:27 +0530

    few files added

--- string.java
+++ string.java
@@ -5,7 +5,7 @@ public class string{
 >>>>>>> cff350c59447d56c4693ceaa26402f9d1b5bd1c3:string.java
     public static void main(String args[]){
         String name = "saurabh";
-        String surname = "dubey";
+        String surname = "dube";
         String fullname = name + " " + surname;
         System.out.println("hello Iam" + " " + fullname);
     }
--- strlen.java
+++ strlen.java
@@ -4,7 +4,7 @@ public class stringmet{
 public class strlen{
 >>>>>>>> cff350c59447d56c4693ceaa26402f9d1b5bd1c3:strlen.java
     public static void main(String args[]){
-        String name = "saurabh";
+        String name = "saurabh dubey";
         System.out.println(name.length());
     }
 }
