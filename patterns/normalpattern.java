b9fc2216aaa8b1e306cd15033d20801f29ac0741
saurabh dubey <dubeysaurabh310@gmail.com>
2025-02-08 20:45:55 +0530

    added patterns folder

--- patterns/normalpattern.java
+++ patterns/normalpattern.java
@@ -3,8 +3,8 @@ public class normalpattern {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++)
-                System.out.print(j + " ");
+            for (int j = 1; j <=n; j++)
+                System.out.print(" * ");
             System.out.println();
         }
     }
