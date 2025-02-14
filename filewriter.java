f2f18edd90da1cb9a2a03fecfe9c3411e5ba41e8
saurabh dubey <dubeysaurabh310@gmail.com>
2025-02-14 08:26:31 +0530

    filewriter io exceptions

---
+++ filewriter.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+
+public class filewriter{
+  public static void main(String[] args) {
+    try(FileWriter writer = new FileWriter("output.txt")) {
+      writer.write("man are brave");  
+      System.out.println("File writer created");
+    }catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
