65c284a46047c0e02dc308ad78725ba57431837c
saurabh dubey <dubeysaurabh310@gmail.com>
2025-02-16 20:50:59 +0530

    filereader files added

---
+++ filereader.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class filereader {
+  public static void main(String[] args) {
+    String filepath = "output.txt";
+    try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+      int data = reader.read();
+      while(data != -1){
+        System.out.print((char)data);
+        data = reader.read();
+      }
+      }catch(Exception e){
+        System.out.println(e);
+     
+  }
+}
+} 
